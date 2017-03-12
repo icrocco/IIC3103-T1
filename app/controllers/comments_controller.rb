be4932f420287aba05b570a2f6e0d75e1487f15a
icrocco <ijcrocco@uc.cl>
2017-03-11 21:53:28 -0300

    comments full working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
-    @comment2 = @press_release.comments.build
+    @comment = Comment.new
   end
 
   # GET /comments/1/edit
@@ -30,7 +30,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @press_release, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
